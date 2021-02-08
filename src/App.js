01762c9c3a0710ae01c0286cf1875815611c383a
Suman M R <suman@anattadesign.com>
2021-02-08 12:57:37 +0530

    Implemented auto suggestion search input

--- src/App.js
+++ src/App.js
@@ -9,24 +9,20 @@ import axios from 'axios';
 //import components
 import SearchBar from './components/SearchBar';
 import TableView from './components/TableView';
+
 function App () {
-  const [sugesstions, setSuggestions] = useState( [] );
+  const [suggestions, setSuggestions] = useState( [] );
   useEffect( () => {
     axios.get( 'https://api.mocki.io/v1/5bb19e07' )
       .then( ( res ) => {
         setSuggestions( res.data.data )
-        console.log( 'sugesstions', sugesstions )
+        console.log( 'suggestions', suggestions )
       } )
       .catch( ( err ) => console.log( err ) );
   }, [] )
   return (
     <div className="App">
-      {
-        sugesstions.map( ( text, index ) => {
-          return ( <p key={text.id}>{text.name}</p> )
-        } )
-      }
-      <SearchBar />
+      <SearchBar suggestions={suggestions} />
       <TableView />
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchBar = () => {
-  return ( <p>Search</p> )
+const SearchBar = ( { suggestions } ) => {
+  const [filteredSuggestions, setFilteredSuggestions] = useState( [] );
+  const filterList = ( e ) => {
+    if ( e.target.value === '' ) {
+      setFilteredSuggestions( [] );
+      return
+    }
+    let filteredData = suggestions.filter( ( text ) => {
+      return text.name.toLowerCase().indexOf( e.target.value.toLowerCase() ) > -1;
+    } );
+    setFilteredSuggestions( filteredData );
+  }
+  return ( <div>
+    <input onChange={( e ) => filterList( e )} type="text" />
+    <ul>
+      {
+        filteredSuggestions.map( ( text, index ) => {
+          return ( <li key={index}>{text.name}</li> )
+        } )
+      }
+    </ul>
+  </div> )
 }
 
 export default SearchBar;
